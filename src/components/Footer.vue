007b60960e7f3bca242dc3e642d8487ca1f4caaa
Samvel <movetonjkee@gmail.com>
2023-02-14 21:38:18 +0300

    vue-cli2

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="container">
-		<p>Footer</p>
+		<p>Footer!</p>
 	</div>
 
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="container">
-		<p>Header</p>
+		<p>Header!</p>
 	</div>
 
 </template>

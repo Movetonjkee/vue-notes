2b74db7c370d9249983567477e89cc7ce7ee93ed
Samvel <movetonjkee@gmail.com>
2023-02-16 21:44:27 +0300

    header

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,21 @@
 <template>
-	<div class="container">
-		<p>Footer!</p>
-	</div>
+	<footer class="footer">
+		<div class="container">
+			<div class="navbar-content">
+				<ul class="navbar-list">
+					<li class="navbar-item">
+						<a href="/about" class="navbar-link">
+							about
+						</a>
+					</li>
+					<li class="navbar-item">
+						<a href="/about" class="navbar-link">
+							home
+						</a>
+					</li>
+				</ul>
+			</div>	
 
+		</div>
+	</footer>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,25 @@
 <template>
-	<div class="container">
-		<p>Header!</p>
-	</div>
+
+	<header class="navbar">
+		<div class="container">
+			<div class="navbar-content">
+				<a href="/" class="navbar-logo">logo</a>
+				<ul class="navbar-list">
+					<li class="navbar-item">
+						<a href="/about" class="navbar-link">
+							about
+						</a>
+					</li>
+					<li class="navbar-item">
+						<a href="/about" class="navbar-link">
+							home
+						</a>
+					</li>
+				</ul>
+			</div>	
+
+		</div>
+	</header>
+
 
 </template>

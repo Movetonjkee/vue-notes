975b03aa68d9c702c147c8d89f1743f1fb5c52f8
Samvel <movetonjkee@gmail.com>
2023-03-05 18:48:41 +0300

    seeders

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -18,27 +18,13 @@
 
 <script>
 import Card from '@/components/UI/Card'
+import items from '@/seeders/items.js'
 
 export default {
   components: { Card },
   data() {
     return {
-      items: [
-        {
-          id: 1,
-          title: 'archer',
-          descr: 'archer archer archer archer archer',
-          img: require('@/assets/img/archer.png'),
-          lvl: 4
-        },
-        {
-          id: 2,
-          title: 'wizard',
-          descr: 'wizard wizard wizard wizard wizard',
-          img: require('@/assets/img/wizard.png'),
-          lvl: 5
-        }
-      ]
+      items: items
     }
   }
 }
---
+++ src/seeders/items.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    title: 'archer',
+    descr: 'archer archer archer archer archer',
+    img: require('@/assets/img/archer.png'),
+    lvl: 4
+  },
+  {
+    id: 2,
+    title: 'wizard',
+    descr: 'wizard wizard wizard wizard wizard',
+    img: require('@/assets/img/wizard.png'),
+    lvl: 5
+  },
+  {
+    id: 3,
+    title: 'goblin',
+    descr: 'goblin goblin goblin goblin goblin',
+    img: require('@/assets/img/goblin.png'),
+    lvl: 5
+  }
+]
